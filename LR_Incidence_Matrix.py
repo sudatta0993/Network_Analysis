d97f6d0ca1f5cd8f3c5da7c57da1ea5da153e1f3
Sudatta Mohanty <sudatta.mohanty@berkeley.edu>
2015-05-23 20:29:01 -0700

    Update LR_Incidence_Matrix.py

--- LR_Incidence_Matrix.py
+++ LR_Incidence_Matrix.py
@@ -1,13 +1,20 @@
+'''
+Created on Dec 2, 2014
+
+@author: sudatta0993
+'''
 #This program evaluates the shortest-path link-route incidence matrix for a given network and stores it in a .csv file
+#Algorithm: Generate shortest path between all nodes in the network. For each path, iterate through all links and check if link falls within shortest path. If it does, then add 1. Else, add 0.
 #Inputs: .csv file with info on all links in the network i.e. linkID, fromNodeID, toNodeID 
 
 #Imports
 import networkx as nx
 import numpy as np
 import csv
+import sys
 
-#Importing data from .csv file (linkID, fromNodeID, toNodeID respectively)
-data = np.genfromtxt('C:\Users\Sudatta\Downloads\Simplified_network_final_actual_counts.csv', delimiter=',', skiprows=1, usecols=(1,26,27))
+#Importing data from .csv file (columns linkID, fromNodeID, toNodeID respectively), file link specified as a command line argument
+data = np.genfromtxt(sys.argv[1], delimiter=',', skiprows=1, usecols=(1,26,27))
 
 #Defining the network using a graph
 G=nx.Graph()
